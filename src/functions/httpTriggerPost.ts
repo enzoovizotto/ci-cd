48b23bfa37f12c48b3c56060488db0aa52b673c7
Enzo Vizotto <enzo.vizotto@tech4h.com.br>
2024-10-03 10:26:20 -0300

    remove steps

--- src/functions/httpTriggerPost.ts
+++ src/functions/httpTriggerPost.ts
@@ -1,6 +1,6 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 
-export async function httpTrigger1(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+export async function httpTriggerPost(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
 
     const name = request.query.get('name') || await request.text() || 'world';
@@ -8,8 +8,11 @@ export async function httpTrigger1(request: HttpRequest, context: InvocationCont
     return { body: `Hello, ${name}!` };
 };
 
-app.http('httpTrigger1', {
-    methods: ['GET', 'POST'],
+app.http('httpTriggerPost', {
+    methods: ['POST'],
     authLevel: 'anonymous',
-    handler: httpTrigger1
+    handler: httpTriggerPost,
+    route:"post"
 });
+
+
